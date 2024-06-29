b5a0954975b83fa7b00326917211398598c11375
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-06-29 11:01:29 +0530

    chapter creation and chapter description

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_comp/ChaptersForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_comp/ChaptersForm.tsx
@@ -14,11 +14,12 @@ import {
 } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
 import { toast } from "sonner"
-import { Pencil, PlusCircle } from "lucide-react"
+import { Loader2, Pencil, PlusCircle } from "lucide-react"
 import { useState } from "react"
 import { cn } from "@/lib/utils"
 import { Chapter, Course } from "@prisma/client"
 import { Input } from "@/components/ui/input"
+import ChaptersList from "./ChaptersList"
 
 interface ChaptersFormProps{
     initialData:Course & {chapters: Chapter[]};
@@ -51,10 +52,32 @@ const onSubmit = async(values:z.infer<typeof formSchema>)=>{
     }
     
 }
+const onReorder = async (updateData: {id:string, position:number}[])=>{
+    try{
+        setIsUpdating(true)
+        await axios.put(`/api/courses/${courseId}/chapters/reorder`,{
+            list:updateData
+        })
+        toast.success("Chapters reordered")
+        router.refresh()
+    }catch{
+        toast.error("Something went wrong");
+    }finally{
+        setIsUpdating(false);
+    }
+}
+const onEdit=(id:string)=>{
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`)
+}
 
 
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+        {isUpdating && (
+            <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+                <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+            </div>
+        )}
         <div className="font-medium flex items-center justify-between">
             Course Chapters
             <Button onClick={toggleCreating} variant={"ghost"}>
@@ -95,7 +118,7 @@ const onSubmit = async(values:z.infer<typeof formSchema>)=>{
                         disabled={!isValid || isSubmitting}
                         type="submit"
                     >   
-                        Creating
+                        Create
                     </Button>
             </form>
             </Form>
@@ -106,6 +129,11 @@ const onSubmit = async(values:z.infer<typeof formSchema>)=>{
                 !initialData.chapters.length && "text-slate-500 italic"
             )}>
                 {!initialData.chapters.length && "No chapters"}
+                <ChaptersList
+                    onEdit={onEdit}
+                    onReorder={onReorder}
+                    items={initialData.chapters || []}
+                />
             </div>
         )}
         {
